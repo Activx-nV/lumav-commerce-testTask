5d00c4da71deb9fad1b2589f9241d69fdcfe581e
Lauri Kammonen <laurikamm@gmail.com>
2021-12-08 18:27:33 +0200

    create markup/styles + separate components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import 'normalize.css';
 import './App.css';
+import ProductInfo from './components/ProductInfo/ProductInfo';
+import ProductsList from './components/ProductsList/ProductsList';
 
 function App() {
   return (
     <div className="products">
-      <div className="product-info">
-        <div className="product-form-section">
-          <h2>Add product form</h2>
-          <form className="product-form">
-            <input type="text" placeholder="Product image" />
-            <input type="text" placeholder="Product name" />
-            <input type="text" placeholder="Product price" />
-            <button className="submit-product">Submit</button>
-          </form>
-        </div>
-        <div className="products-cart-counter">
-          <p>Products in cart: 0</p>
-        </div>
-      </div>
+      <ProductInfo />
 
       {/*products are hardcoded right now to create a conception-*/}
-      <div className="products-list">
-        <div className="product-item">
-          <img src="img-url-state-will-be-here" alt="product item" />
-          <div>Product name</div>
-          <div>Product price</div>
-          <button className="add-product-to-cart">Add to cart</button>
-          <button className="remove-product-to-cart">Remove from the cart</button>
-        </div>
-      </div>
+      <ProductsList />
     </div>
   );
 }
---
+++ src/components/ProductsList/ProductsList.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import './ProductsList.css';
+
+const ProductsList = () => {
+  return (
+    <div className="products-list">
+      <div className="product-item">
+        <img
+          src="https://i.playground.ru/p/CRLGFteHXlRy20nft5w16Q.jpeg"
+          alt="product item"
+          className="product-image"
+        />
+        <p className="product-name">Product name</p>
+        <p className="product-price">Product price</p>
+        <button className="add-product-to-cart">Add to cart</button>
+        <button className="remove-product-to-cart">Remove from the cart</button>
+      </div>
+      
+      <div className="product-item">
+        <img
+          src="https://i.playground.ru/p/CRLGFteHXlRy20nft5w16Q.jpeg"
+          alt="product item"
+          className="product-image"
+        />
+        <p className="product-name">Product name</p>
+        <p className="product-price">Product price</p>
+        <button className="add-product-to-cart">Add to cart</button>
+        <button className="remove-product-to-cart">Remove from the cart</button>
+      </div>
+
+      <div className="product-item">
+        <img
+          src="https://i.playground.ru/p/CRLGFteHXlRy20nft5w16Q.jpeg"
+          alt="product item"
+          className="product-image"
+        />
+        <p className="product-name">Product name</p>
+        <p className="product-price">Product price</p>
+        <button className="add-product-to-cart">Add to cart</button>
+        <button className="remove-product-to-cart">Remove from the cart</button>
+      </div>
+
+      <div className="product-item">
+        <img
+          src="https://i.playground.ru/p/CRLGFteHXlRy20nft5w16Q.jpeg"
+          alt="product item"
+          className="product-image"
+        />
+        <p className="product-name">Product name</p>
+        <p className="product-price">Product price</p>
+        <button className="add-product-to-cart">Add to cart</button>
+        <button className="remove-product-to-cart">Remove from the cart</button>
+      </div>
+
+      <div className="product-item">
+        <img
+          src="https://i.playground.ru/p/CRLGFteHXlRy20nft5w16Q.jpeg"
+          alt="product item"
+          className="product-image"
+        />
+        <p className="product-name">Product name</p>
+        <p className="product-price">Product price</p>
+        <button className="add-product-to-cart">Add to cart</button>
+        <button className="remove-product-to-cart">Remove from the cart</button>
+      </div>
+
+      <div className="product-item">
+        <img
+          src="https://i.playground.ru/p/CRLGFteHXlRy20nft5w16Q.jpeg"
+          alt="product item"
+          className="product-image"
+        />
+        <p className="product-name">Product name</p>
+        <p className="product-price">Product price</p>
+        <button className="add-product-to-cart">Add to cart</button>
+        <button className="remove-product-to-cart">Remove from the cart</button>
+      </div>
+
+      <div className="product-item">
+        <img
+          src="https://i.playground.ru/p/CRLGFteHXlRy20nft5w16Q.jpeg"
+          alt="product item"
+          className="product-image"
+        />
+        <p className="product-name">Product name</p>
+        <p className="product-price">Product price</p>
+        <button className="add-product-to-cart">Add to cart</button>
+        <button className="remove-product-to-cart">Remove from the cart</button>
+      </div>
+
+      <div className="product-item">
+        <img
+          src="https://i.playground.ru/p/CRLGFteHXlRy20nft5w16Q.jpeg"
+          alt="product item"
+          className="product-image"
+        />
+        <p className="product-name">Product name</p>
+        <p className="product-price">Product price</p>
+        <button className="add-product-to-cart">Add to cart</button>
+        <button className="remove-product-to-cart">Remove from the cart</button>
+      </div>
+      
+    </div>
+  );
+};
+
+export default ProductsList;
